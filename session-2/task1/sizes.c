ce9060177ec61125e21858d5f74e28b7439809f7
izzy2611 <nrzt0740@leeds.ac.uk>
2026-02-25 11:53:34 +0000

    week 2.3

--- session-2/task1/sizes.c
+++ session-2/task1/sizes.c
@@ -11,10 +11,18 @@ int main( void ) {
     char testChar;
     int testInt;
     float testFloat;
+    short testshortInt;
+    long testLongInt;
+    double testDouble;
+
+    
 
     printf("Char size is %ld bytes\n",sizeof(testChar));
     printf("Int size is %ld bytes\n",sizeof(testInt));
     printf("Float size is %ld bytes\n",sizeof(testFloat));
+    printf("Float size is %ld bytes\n",sizeof(testshortInt));
+    printf("Float size is %ld bytes\n",sizeof(testLongInt));
+    printf("Float size is %ld bytes\n",sizeof(testDouble));
 
     return 0;
 }
--- worksheet/portfolio/salary.c
+++ worksheet/portfolio/salary.c
@@ -3,21 +3,34 @@
 
 /*
  * Potrfolio submission
- * Name:
- * ID:
+ * Name: Izzy Shaw
+ * ID: 201956339
  */
 
  int main( void ) {
 
     // define and initialise variables for the problem data 
-
+   double salary = 30000;
+   int national_insurance = 9;
+   int tax = 25;
+   int total_salary;
+   double tax_contribution;
     // calculate the deductions and final take-home salary
+   double NI_contribution = salary * (national_insurance / 100.0);
+   double salary_after_NI = salary - NI_contribution;
+   double taxable_amount = salary_after_NI-12500;
+   if (taxable_amount > 0) {
+          tax_contribution = taxable_amount * (tax / 100.0);
+}
 
+   double take_home_salary = salary_after_NI - tax_contribution;
+ 
     // Use only these print statement with appropriate formatting and variable names
-    //printf("Salary £...",var_name);
-    //printf("NI contribution £...",var_name);
-    //printf("Tax contribution £...",var_name);
-    //printf("Take home salary £...",var_name);
+
+   printf("Salary £%.2f",salary);
+   printf("NI contribution £%.2f",NI_contribution);
+   printf("Tax contribution £%.2f",tax_contribution);
+   printf("Take home salary £%.2f",take_home_salary);
 
     return 0;
  }
